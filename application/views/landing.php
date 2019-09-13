e7d894000ac72b448eb8fa2b7991b5abf33569d7
Shalom Orlando Otieno <orlandostudies@gmail.com>
2019-09-13 12:59:38 +0300

    refactor the landing page for presentation

--- application/views/landing.php
+++ application/views/landing.php
@@ -5,12 +5,13 @@
     <!-- Required meta tags -->
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-
+    
     <!-- Bootstrap CSS -->
-    <link rel="stylesheet" href="css/bootstrap.min.css">
-
+    
+    <link href="<?php echo base_url();?>css/bootstrap.min.css" type="text/css" rel="stylesheet">
     <!-- External CSS -->
-    <link rel="stylesheet" type="text/css" href="./css/style.css">
+    <link href="<?php echo base_url();?>css/style.css" type="text/css" rel="stylesheet">
+    
 
     <title>STRATHMORE E CAFE | Home</title>
 
@@ -21,7 +22,7 @@
   <nav class="navbar navbar-expand-md bg-success navbar-light ">
 
   <!-- Brand Logo -->
-  <a class="navbar-brand text-dark" href="index.html"><img src="strath.png" height="70px" width="70px">Strathmore University E-Cafe</a>
+  <a class="navbar-brand text-dark" href="index.html"><img src="<?php echo base_url();?>Images/strath.png" height="70px" width="70px">Strathmore University E-Cafe</a>
 
   <!-- Toggler/collapsibe Button/Hamburger Menu -->
   <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#collapsibleNavbar">
@@ -62,13 +63,13 @@
       
     </div>
     <div class="carousel-item active">
-      <img class="d-block w-100" src="main1.jpg" alt="First slide">
+      <img class="d-block w-100" src="<?php echo base_url();?>Images/main1.jpg" alt="First slide">
     </div>
     <div class="carousel-item">
-      <img class="d-block w-100" src="main.jpg" alt="Second slide">
+      <img class="d-block w-100" src="<?php echo base_url();?>Images/main.jpg" alt="Second slide">
     </div>
     <div class="carousel-item">
-      <img class="d-block w-100" src="main2.jpg" alt="Third slide">
+      <img class="d-block w-100" src="<?php echo base_url();?>Images/main2.jpg" alt="Third slide">
     </div>
   </div>
 
@@ -90,7 +91,7 @@
 
 <div class="card-deck">
   <div class="card">
-    <img class="card-img-top" src="food1.png" alt="Card image cap">
+    <img class="card-img-top" src="<?php echo base_url();?>Images/food1.png" alt="Card image cap">
     <div class="card-body">
       <h5 class="card-title">Rice and Meatballs combo</h5>
       <p class="card-text">Lorem  ipsum</p>
@@ -100,7 +101,7 @@
   </div>
 
   <div class="card">
-    <img class="card-img-top" src="food5.jpg" alt="Card image cap">
+    <img class="card-img-top" src="<?php echo base_url();?>Images/food5.jpg" alt="Card image cap">
     <div class="card-body">
       <h5 class="card-title">Rice</h5>
       <p class="card-text">Plain rice .</p>
@@ -109,7 +110,7 @@
   </div>
 
   <div class="card">
-    <img class="card-img-top" src="food8.jpg" alt="Card image cap">
+    <img class="card-img-top" src="<?php echo base_url();?>Images/food8.jpg" alt="Card image cap">
     <div class="card-body">
       <h5 class="card-title">Pilau</h5>
       <p class="card-text">Chef's pilau</p>
@@ -118,7 +119,7 @@
   </div>
 
   <div class="card">
-    <img class="card-img-top" src="food3.png" alt="Card image cap">
+    <img class="card-img-top" src="<?php echo base_url();?>Images/food3.png" alt="Card image cap">
     <div class="card-body">
       <h5 class="card-title">Strathmore Club burger</h5>
       <p class="card-text">The burger has fresh lettuce</p>
@@ -129,7 +130,7 @@
 
 <div class="card-deck">
   <div class="card">
-    <img class="card-img-top" src="food2.png" alt="Card image cap">
+    <img class="card-img-top" src="<?php echo base_url();?>Images/food2.png" alt="Card image cap">
     <div class="card-body">
       <h5 class="card-title">Ugali and Fish Combo</h5>
       <p class="card-text">Lorem  ipsum</p>
@@ -139,7 +140,7 @@
   </div>
 
   <div class="card">
-    <img class="card-img-top" src="food4.png" alt="Card image cap">
+    <img class="card-img-top" src="<?php echo base_url();?>Images/food4.png" alt="Card image cap">
     <div class="card-body">
       <h5 class="card-title">Fried Chicken</h5>
       <p class="card-text">Plain rice .</p>
@@ -148,7 +149,7 @@
   </div>
 
   <div class="card">
-    <img class="card-img-top" src="food9.jpg" alt="Card image cap" height="208px">
+    <img class="card-img-top" src="<?php echo base_url();?>Images/food9.jpg" alt="Card image cap" height="208px">
     <div class="card-body">
       <h5 class="card-title">Pasta</h5>
       <p class="card-text">Lorem ipsum</p>
@@ -157,7 +158,7 @@
   </div>
 
   <div class="card">
-    <img class="card-img-top" src="food7.jpg" alt="Card image cap" >
+    <img class="card-img-top" src="<?php echo base_url();?>Images/food7.jpg" alt="Card image cap" >
     <div class="card-body">
       <h5 class="card-title">Pizza</h5>
       <p class="card-text">Lorem Ipsum</p>
@@ -184,9 +185,9 @@
       <div class="col-md-3 col-lg-4 col-xl-3 mx-auto mb-4">
 
         <!-- Content -->
-        <h6 class="text-uppercase font-weight-bold">Company name</h6>
+        <h6 class="text-uppercase font-weight-bold">Strathmore University</h6>
         <hr class="deep-purple accent-2 mb-4 mt-0 d-inline-block mx-auto" style="width: 60px;">
-        <p>Here you can use rows and columns to organize your footer content. Lorem ipsum dolor sit amet,
+        <p> Lorem ipsum dolor sit amet,
           consectetur
           adipisicing elit.</p>
 
@@ -197,21 +198,15 @@
       <div class="col-md-2 col-lg-2 col-xl-2 mx-auto mb-4">
 
         <!-- Links -->
-        <h6 class="text-uppercase font-weight-bold">Products</h6>
+        <h6 class="text-uppercase font-weight-bold">Other links</h6>
         <hr class="deep-purple accent-2 mb-4 mt-0 d-inline-block mx-auto" style="width: 60px;">
         <p>
-          <a href="#!">MDBootstrap</a>
+          <a href="#!">Privacy Policy</a>
         </p>
         <p>
-          <a href="#!">MDWordPress</a>
+          <a href="#!">Terms of use</a>
         </p>
-        <p>
-          <a href="#!">BrandFlow</a>
-        </p>
-        <p>
-          <a href="#!">Bootstrap Angular</a>
-        </p>
-
+        
       </div>
       <!-- Grid column -->
 
@@ -222,16 +217,16 @@
         <h6 class="text-uppercase font-weight-bold">Useful links</h6>
         <hr class="deep-purple accent-2 mb-4 mt-0 d-inline-block mx-auto" style="width: 60px;">
         <p>
-          <a href="#!">Your Account</a>
+          <a href="#!">Payments</a>
         </p>
         <p>
-          <a href="#!">Become an Affiliate</a>
+          <a href="#!">About us</a>
         </p>
         <p>
-          <a href="#!">Shipping Rates</a>
+          <a href="#!">Menu</a>
         </p>
         <p>
-          <a href="#!">Help</a>
+          <a href="#!">Signin</a>
         </p>
 
       </div>
@@ -244,13 +239,13 @@
         <h6 class="text-uppercase font-weight-bold">Contact</h6>
         <hr class="deep-purple accent-2 mb-4 mt-0 d-inline-block mx-auto" style="width: 60px;">
         <p>
-          <i class="fas fa-home mr-3"></i> Nairobi, NB 10012, KE</p>
+          <i class="fas fa-home mr-3"></i> Nairobi, NB 100, KE</p>
         <p>
-          <i class="fas fa-envelope mr-3"></i> info@example.com</p>
+          <i class="fas fa-envelope mr-3"></i> infoStrathmore@.edu</p>
         <p>
-          <i class="fas fa-phone mr-3"></i> + 01 234 567 88</p>
+          <i class="fas fa-phone mr-3"></i> + 020 234 567 88</p>
         <p>
-          <i class="fas fa-print mr-3"></i> + 01 234 567 89</p>
+          <i class="fas fa-print mr-3"></i> + 020 234 567 89</p>
 
       </div>
       <!-- Grid column -->
@@ -274,8 +269,8 @@
 
     <!-- Optional JavaScript -->
     <!-- jQuery first, then Popper.js, then Bootstrap JS -->
-    <script src="js/jquery.min.js" ></script>
-    <script src="js/popper.min.js" ></script>
-    <script src="js/bootstrap.min.js" ></script>
+    <script src="<?php echo base_url();?>js/jquery.min.js" ></script>
+    <script src="<?php echo base_url();?>js/popper.min.js" ></script>
+    <script src="<?php echo base_url();?>js/bootstrap.min.js" ></script>
   </body>
 </html>
