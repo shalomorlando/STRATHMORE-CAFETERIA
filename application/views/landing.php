02411ea8ff37e867db5defc82bd6ecd9501bca3d
Alphonce Mutebi <mutebialphonce@gmail.com>
2019-09-13 10:48:56 +0300

    Added landing Page

---
+++ application/views/landing.php
@@ -0,0 +1,280 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="css/bootstrap.min.css">
+
+    <!-- External CSS -->
+    <link rel="stylesheet" type="text/css" href="style.css">
+
+    <title>STRATHMORE E CAFE | Home</title>
+
+  </head>
+  <body>
+
+  <!-- Navigation bar -->
+  <nav class="navbar navbar-expand-md bg-success navbar-light ">
+
+  <!-- Brand Logo -->
+  <a class="navbar-brand text-dark" href="index.html"><img src="strath.png" height="70px" width="70px">Strathmore University E-Cafe</a>
+
+  <!-- Toggler/collapsibe Button/Hamburger Menu -->
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#collapsibleNavbar">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+
+  <!-- Navbar links -->
+  <div class="collapse navbar-collapse " id="collapsibleNavbar">
+    <ul class="navbar-nav ml-auto">
+      <li class="nav-item">
+        <a class="nav-link text-dark " href="#" >Home</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link text-dark" href="#">Menu</a>
+      </li> 
+      <li class="nav-item">
+        <a class="nav-link text-dark" href="#">About us </a>
+      </li>
+       <li class="nav-item">
+        <a class="nav-link text-dark" href="#">Signin</a>
+      </li> 
+    </ul>
+  </div> 
+</nav>
+
+
+
+<div id="carouselExampleIndicators" class="carousel slide" data-ride="carousel">
+
+
+  <ol class="carousel-indicators">
+    <li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
+    <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
+    <li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
+  </ol>
+  <div class="carousel-inner">
+    <div class="">
+      
+    </div>
+    <div class="carousel-item active">
+      <img class="d-block w-100" src="main1.jpg" alt="First slide">
+    </div>
+    <div class="carousel-item">
+      <img class="d-block w-100" src="main.jpg" alt="Second slide">
+    </div>
+    <div class="carousel-item">
+      <img class="d-block w-100" src="main2.jpg" alt="Third slide">
+    </div>
+  </div>
+
+  <a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
+    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+    <span class="sr-only">Previous</span>
+  </a>
+  <a class="carousel-control-next" href="#carouselExampleIndicators" role="button" data-slide="next">
+    <span class="carousel-control-next-icon" aria-hidden="true"></span>
+    <span class="sr-only">Next</span>
+  </a>
+
+</div>
+
+
+  <div>
+    <p class="fontstyle">  Menu</p>
+  </div>
+
+<div class="card-deck">
+  <div class="card">
+    <img class="card-img-top" src="food1.png" alt="Card image cap">
+    <div class="card-body">
+      <h5 class="card-title">Rice and Meatballs combo</h5>
+      <p class="card-text">Lorem  ipsum</p>
+      
+        
+    </div>
+  </div>
+
+  <div class="card">
+    <img class="card-img-top" src="food5.jpg" alt="Card image cap">
+    <div class="card-body">
+      <h5 class="card-title">Rice</h5>
+      <p class="card-text">Plain rice .</p>
+     
+    </div>
+  </div>
+
+  <div class="card">
+    <img class="card-img-top" src="food8.jpg" alt="Card image cap">
+    <div class="card-body">
+      <h5 class="card-title">Pilau</h5>
+      <p class="card-text">Chef's pilau</p>
+      
+    </div>
+  </div>
+
+  <div class="card">
+    <img class="card-img-top" src="food3.png" alt="Card image cap">
+    <div class="card-body">
+      <h5 class="card-title">Strathmore Club burger</h5>
+      <p class="card-text">The burger has fresh lettuce</p>
+     
+    </div>
+  </div>
+</div>
+
+<div class="card-deck">
+  <div class="card">
+    <img class="card-img-top" src="food2.png" alt="Card image cap">
+    <div class="card-body">
+      <h5 class="card-title">Ugali and Fish Combo</h5>
+      <p class="card-text">Lorem  ipsum</p>
+      
+        
+    </div>
+  </div>
+
+  <div class="card">
+    <img class="card-img-top" src="food4.png" alt="Card image cap">
+    <div class="card-body">
+      <h5 class="card-title">Fried Chicken</h5>
+      <p class="card-text">Plain rice .</p>
+     
+    </div>
+  </div>
+
+  <div class="card">
+    <img class="card-img-top" src="food9.jpg" alt="Card image cap" height="208px">
+    <div class="card-body">
+      <h5 class="card-title">Pasta</h5>
+      <p class="card-text">Lorem ipsum</p>
+      
+    </div>
+  </div>
+
+  <div class="card">
+    <img class="card-img-top" src="food7.jpg" alt="Card image cap" >
+    <div class="card-body">
+      <h5 class="card-title">Pizza</h5>
+      <p class="card-text">Lorem Ipsum</p>
+     
+    </div>
+  </div>
+</div>
+
+
+
+
+
+<!-- Footer -->
+<footer class="page-footer font-small unique-color-dark">
+
+ 
+  <!-- Footer Links -->
+  <div class="container text-center text-md-left mt-5">
+
+    <!-- Grid row -->
+    <div class="row mt-3">
+
+      <!-- Grid column -->
+      <div class="col-md-3 col-lg-4 col-xl-3 mx-auto mb-4">
+
+        <!-- Content -->
+        <h6 class="text-uppercase font-weight-bold">Company name</h6>
+        <hr class="deep-purple accent-2 mb-4 mt-0 d-inline-block mx-auto" style="width: 60px;">
+        <p>Here you can use rows and columns to organize your footer content. Lorem ipsum dolor sit amet,
+          consectetur
+          adipisicing elit.</p>
+
+      </div>
+      <!-- Grid column -->
+
+      <!-- Grid column -->
+      <div class="col-md-2 col-lg-2 col-xl-2 mx-auto mb-4">
+
+        <!-- Links -->
+        <h6 class="text-uppercase font-weight-bold">Products</h6>
+        <hr class="deep-purple accent-2 mb-4 mt-0 d-inline-block mx-auto" style="width: 60px;">
+        <p>
+          <a href="#!">MDBootstrap</a>
+        </p>
+        <p>
+          <a href="#!">MDWordPress</a>
+        </p>
+        <p>
+          <a href="#!">BrandFlow</a>
+        </p>
+        <p>
+          <a href="#!">Bootstrap Angular</a>
+        </p>
+
+      </div>
+      <!-- Grid column -->
+
+      <!-- Grid column -->
+      <div class="col-md-3 col-lg-2 col-xl-2 mx-auto mb-4">
+
+        <!-- Links -->
+        <h6 class="text-uppercase font-weight-bold">Useful links</h6>
+        <hr class="deep-purple accent-2 mb-4 mt-0 d-inline-block mx-auto" style="width: 60px;">
+        <p>
+          <a href="#!">Your Account</a>
+        </p>
+        <p>
+          <a href="#!">Become an Affiliate</a>
+        </p>
+        <p>
+          <a href="#!">Shipping Rates</a>
+        </p>
+        <p>
+          <a href="#!">Help</a>
+        </p>
+
+      </div>
+      <!-- Grid column -->
+
+      <!-- Grid column -->
+      <div class="col-md-4 col-lg-3 col-xl-3 mx-auto mb-md-0 mb-4">
+
+        <!-- Links -->
+        <h6 class="text-uppercase font-weight-bold">Contact</h6>
+        <hr class="deep-purple accent-2 mb-4 mt-0 d-inline-block mx-auto" style="width: 60px;">
+        <p>
+          <i class="fas fa-home mr-3"></i> Nairobi, NB 10012, KE</p>
+        <p>
+          <i class="fas fa-envelope mr-3"></i> info@example.com</p>
+        <p>
+          <i class="fas fa-phone mr-3"></i> + 01 234 567 88</p>
+        <p>
+          <i class="fas fa-print mr-3"></i> + 01 234 567 89</p>
+
+      </div>
+      <!-- Grid column -->
+
+    </div>
+    <!-- Grid row -->
+
+  </div>
+  <!-- Footer Links -->
+
+  <!-- Copyright -->
+  <div class="footer-copyright text-center py-3">© 2019 Copyright:
+    <a href="#"> StrathmoreE-Cafe.com</a>
+  </div>
+  <!-- Copyright -->
+
+</footer>
+<!-- Footer -->
+
+
+
+    <!-- Optional JavaScript -->
+    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+    <script src="js/jquery.min.js" ></script>
+    <script src="js/popper.min.js" ></script>
+    <script src="js/bootstrap.min.js" ></script>
+  </body>
+</html>
